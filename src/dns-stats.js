67b8385dab66b1f2eb157b5f436fc514d5c0198a
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-12-24 17:20:20 +0300

    feat: add getDNSStats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const allDoms = new Set();
+  const resObj = {};
+  domains
+    .map((elem) => elem.split("."))
+    .map((elem) => {
+      let acc = "";
+      for (let i = elem.length - 1; i >= 0; i--) {
+        acc += "." + elem[i];
+        console.log(acc);
+        allDoms.add(acc);
+      }
+    });
+
+  Array.from(allDoms).forEach((elem) => {
+    resObj[`${elem}`] = 0;
+  });
+
+  for (let key in resObj) {
+    domains.forEach((elem) => {
+      if (elem.indexOf(key.slice(1).split(".").reverse().join(".")) !== -1) {
+        resObj[`${key}`] += 1;
+      }
+    });
+  }
+
+  return resObj;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
